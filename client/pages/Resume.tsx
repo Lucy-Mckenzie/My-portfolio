f1f1e2099690413040bc1a0d03cade4e185dd1e2
Lucy-Mckenzie <lucymckenzie62@gmail.com>
2024-12-09 17:03:38 +1300

    adding resume (#11)

--- client/pages/Resume.tsx
+++ client/pages/Resume.tsx
@@ -1,7 +1,15 @@
 
 export default function Resume() {
   return (
-    <div className='flex justify-center'>
+    <div className='flex justify-center items-center '>
+    
+       <a href="/images/Resume.pdf" download="Resume-Lucy-Mckenzie.pdf">
+        <button className='font-manrope text-lg'>Click to download</button>
+        <div className='w-[600px] h-auto'>
+        <img src='/images/cv.png' alt='cv' />
+        <img src='/images/cv1.png' alt='cv' />
+        </div>
+      </a>
     </div>
 
   )
