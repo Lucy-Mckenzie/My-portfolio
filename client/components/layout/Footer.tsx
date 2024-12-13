c455d1c90f940839154dfda759fa346c05035689
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-13 13:37:18 +1300

    editing background and fonts for main page

--- client/components/layout/Footer.tsx
+++ client/components/layout/Footer.tsx
@@ -9,12 +9,6 @@ export default function Footer({darkMode}: Props) {
   return (
     <div className={`flex flex-col ${darkMode ? 'bg-[#121212] text-white' : 'bg-[#fffbf9] text-black'}`}>
     <footer className='py-8'>
-      <div className='container flex flex-row justify-between items-center font-manrope'>
-        <p className='text-sm pl-32'>
-          LUCY MCKENZIE
-        </p>
-        </div>
-  
       <div className='flex flex-row space-x-8 justify-center items-center font-manrope'>
         <Link to='/about' className='mb-6'>About</Link>
         <Link to='/contact' className='mb-6'>Contact me</Link>
