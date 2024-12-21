88579d48e54a5aed6b4a5ea6a38c8b2574b95cf8
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-22 11:48:17 +1300

    removing unused fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,8 @@ export default {
         white: "0 8px 15px rgba(255, 255, 255, 0.5)", 
       },
       fontFamily: {
-        merriweather: ['Merriweather', 'serif'],
         sans: ['Lato', 'sans-serif'],
         manrope: ['Manrope', 'sans-serif'],
-        playfair: ['Playfair Display', 'serif'],
     },
     darkMode: 'class',
   },
