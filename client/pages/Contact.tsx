14801ad3232b0473cb08808446a9a252a2fd8f93
Lucy Mckenzie <lucymckenzie62@gmail.com>
2024-12-29 17:16:36 +1300

    Adding functionality to contact page

--- client/pages/Contact.tsx
+++ client/pages/Contact.tsx
@@ -1,9 +1,12 @@
+import { motion } from 'framer-motion'
+import { useState } from 'react'
 import { useOutletContext } from 'react-router-dom'
 
 export default function Contact() {
 
   const email = 'Lucymckenzie62@gmail.com'
- 
+  const [click, setClick] = useState(false)
+
   const handleCopy = () => {
     navigator.clipboard.writeText(email)
     .then(() => alert('Copied!'))
@@ -15,14 +18,18 @@ export default function Contact() {
 
   return (
     <div className={`flex flex-col w-full mx-auto p-6 space-y-6 py-40 items-center justify-center ${darkMode ? 'bg-[#3d4451] text-white' : 'bg-[#fffbf9] text-black'}`}>
-      <div>
-        <h1 className='font-manrope text-3xl text-left'>Get in touch (Click to copy)</h1>
-        <button className='font-manrope text-6xl text-left'
+        <motion.h1 
+        className='font-manrope text-3xl text-left hover:cursor-pointer' 
+        onClick={() => setClick(prev => !prev)} 
+        drag>
+       {click ? "Get in touch (Oops, not here! Try again)" : "Get in touch (Click to copy)"}
+        </motion.h1>
+        <motion.button className='font-manrope text-6xl text-left hover:text-[#459cff]'
         onClick={(handleCopy)}
+        whileTap={{ scale: 0.9 }}
         >
           {email}
-        </button>
-      </div>
+        </motion.button>
     </div>
   )
 } 
