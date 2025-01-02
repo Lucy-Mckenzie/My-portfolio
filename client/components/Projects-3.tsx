9d5cae6b6a686745b15f89868361660e9fb73cf4
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-01-02 19:59:24 +1300

    adding new photos for main

--- client/components/Projects-3.tsx
+++ client/components/Projects-3.tsx
@@ -26,6 +26,10 @@ export default function Projects3() {
               onMouseEnter={() => setHoveredImage('mycelium')}
               onMouseLeave={() => setHoveredImage(null)}
             />
+             <button 
+            className='font-manrope absolute bottom-2 right-4 text-sm z-10 px-4 py-2 bg-white/20 backdrop-blur-md border border-white/40 rounded-full shadow-md transition duration-300 hover:bg-white/30 hover:border-white/60 hover:shadow-lg'>
+              Live Website
+            </button>
             </Link>
             {hoveredImage === 'mycelium' && (
               <MouseHover />
@@ -35,6 +39,14 @@ export default function Projects3() {
             <h1 className=' text-xl font-manrope'>
             <strong>myCelium:</strong> The game you just <em>can’t</em> put down! Whether you’re a <strong>plant lover</strong> or not, myCelium is for everyone. 
             </h1>
+            <Link
+            to='/mycelium'
+            onClick={scrollToTop}
+            >
+            <button className='font-manrope relative top-14 text-md rounded-full border border-black px-2 py-1 bg-black text-white'>
+              View Case Study →
+            </button>
+            </Link>
             </div>
           </div>
     
@@ -61,6 +73,14 @@ export default function Projects3() {
             <h1 className=' text-xl font-manrope'>
           <strong>We Hike:</strong> helps hikers stay prepared with saved trails and huts, real-time alerts, and trail updates—all in one place.
             </h1>
+            <Link
+            to='/wehike'
+            onClick={scrollToTop}
+            >
+            <button className='font-manrope relative top-14 right-1 text-md rounded-full border border-black px-2 py-1 bg-black text-white'>
+              View Case Study →
+            </button>
+            </Link>
             </div>
           </div>
         </div>
