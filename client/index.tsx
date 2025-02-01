88aa0639ade21bfa6dfc5582495f76897924c8d4
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-02-01 18:08:53 +1300

    Reistalling package.

--- client/index.tsx
+++ client/index.tsx
@@ -3,7 +3,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RouterProvider } from 'react-router-dom'
 
-import { router } from './router'
+import { router } from './router.tsx'
 
 const queryClient = new QueryClient()
 
