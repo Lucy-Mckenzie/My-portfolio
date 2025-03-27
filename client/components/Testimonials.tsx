aaded706f31b6b118d0ed4fe358df9009c5dae42
Lucy Mckenzie <lucymckenzie62@gmail.com>
2025-03-27 19:12:16 +1000

    Adding testimonials section.

--- client/components/Testimonials.tsx
+++ client/components/Testimonials.tsx
@@ -4,14 +4,16 @@ const testimonals = [
   {
     id: 1,
     img: '/images/Jaydon.png',
-    name: 'Jaydon',
-    comment: 'Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom'
+    name: 'Jaydon - marketing & project manager',
+    website: 'Suncoast Patios',
+    comment: 'I loved working with Lucy, she took all of my ideas on board and helped with her own expert perspective when necessary. My previous site had issues with indexing and was slow to load, outdated looking and dropping in search ranking. We’re now seeing much better search results, load speed, and conversions. Lucy is great with communication, super easy to work with and I never would’ve ended up with such a clean, fast, functional site without her help and expertise. She knows her craft inside out and I can’t recommend her enough!'
   },
   {
     id: 2,
     img: '/images/Andrew.png',
-    name: 'Andrew',
-    comment: 'Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom Loren Epsom'
+    name: 'Andrew - product owner',
+    website: 'Suncoast Patios',
+    comment: "Lucy was an absolute star! She completely transformed our outdated website, which had been causing ongoing issues and frustration for both us and our customers. Thanks to her expertise, the site is now modern, user-friendly, and functions seamlessly. Our customers have specifically commented on how much easier it is to navigate and how simple the new form is to fill out, making their experience far more enjoyable. Lucy's attention to detail and commitment to delivering a high-quality result exceeded our expectations. We couldn’t recommend her highly enough!"
   }
 ]
 
@@ -26,7 +28,7 @@ const Testimonals = () => {
       </h1>
       <div className='grid grid-cols-2 grid-rows-1 gap-12'>
         {testimonals.map((review, index) => (
-          <div key={index} className='relative flex items-center bg-[#FFC41F]/50 rounded-lg shadow-md text-black flex-col py-5 px-12 text-center max-w-md'>
+          <div key={index} className='relative flex items-center bg-[#638cc0]/30 rounded-lg shadow-md text-black flex-col py-5 px-12 text-center max-w-md'>
             <div className='absolute -top-10'>
               <img 
                 className='w-20 h-20 p-1 object-contain rounded-full shrink-0'
@@ -36,8 +38,11 @@ const Testimonals = () => {
                 height={40}
               />
             </div>
-            <h1 className='text-2xl pt-5 pb-3'>
+            <h1 className='text-2xl pt-5'>
               {review.name}
+            </h1>
+            <h1 className='text-lg font-light'>
+              {review.website}
             </h1>
             <h2 className='text-sm'>
               {review.comment}
